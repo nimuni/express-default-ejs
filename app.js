4ff117e24fd3e0df8202fb9c56967d107007eb0f
nimuni <kym9788@gmail.comi>
2022-01-08 21:29:32 +0900

    feat: morgan logger 형식 변경, 로그 파일시스템에 기록되게 추가

--- app.js
+++ app.js
@@ -2,24 +2,50 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const cors = require("cors")
+const moment = require('moment-timezone');
+const TIMEZONE = "Asia/Seoul";
 
 const rootRouter = require('./routes/root');
 const viewsRouter = require('./routes/views');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// logger settings
+const morgan = require('morgan');
+const rfs = require('rotating-file-stream');
+function logFilename() {
+  return `${moment().tz(TIMEZONE).format("YYYYMMDD")}.log`;
+}
+const accessLogStream = rfs.createStream(logFilename, {
+  interval: '1d',
+  path: path.resolve(__dirname, 'logs'),
+});
+morgan.token('user', (req, res) => {
+  return req.user ? req.user.id : "Guest"
+  // return _.get(req, 'req.user.id') || 'Guest';
+})
+morgan.token('ip', (req, res) => {
+  return req.headers['x-forwarded-for'] || req.connection.remoteAddress
+})
+morgan.token('date', (req, res) => {
+  return moment().tz(TIMEZONE).format();
+})
+morgan.format('customFormat', '[:date][:method][:status][:response-time ms][url=:url][:ip][:user]');
+app.use(morgan('customFormat', {
+  stream: accessLogStream
+}));
+app.use(morgan('customFormat'));
 
 // make db connection
 const mongodb = require(process.cwd()+"/bin/db/mongodb")
@@ -27,6 +53,7 @@ mongodb.connectToServer(()=>{
   console.log("mongo db connected")
 })
 
+// router settings
 app.use('/', rootRouter);
 app.use('/views', viewsRouter);
 
